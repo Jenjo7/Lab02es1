d080383a6741192b4a4ac0b3d8812c8b1bff60c7
Gianni Savini <gianni.savini@studio.unibo.it>
2017-04-05 21:11:09 +0200

    I add comments at my code...

--- app/src/main/java/com/example/giannisavini/lab02/MainActivity.java
+++ app/src/main/java/com/example/giannisavini/lab02/MainActivity.java
@@ -24,18 +24,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        // Creazione dei riferimento con gli elementi della view tramite l'id univoco assegnatoli
         lsvsStates = (ListView) findViewById(R.id.lsv_state);
         //List<State> states = new ArrayList<>();
         String[] states = {"Italia", "Regno Unito", "Francia", "Germania", "Spagana", "Svizzera",
                             "Belgio", "Portogallo", "Austria", "Norvegia", "Svezia", "Finlandia"};
+        /**
+         * Creazione di un'istanza della classe ArrayAdapter. Tramite il costruttore vengono passati alla classe il riferimento all'activity
+         * (tramite la parola chiave this), il nome del layout che definirà l'interfaccia di una singola cella, l'id della TextView all'interno
+         * del suddetto layout che si occuperà della presentazione della stringa, e la collezzione di stringhe da presentare.
+         */
         ArrayAdapter<String> stateAdapter = new ArrayAdapter<>(this, R.layout.list_item_state ,R.id.txv_state, states);
+        //L'adapter appena creato viene passato alla ListView tramite il metodo apposito
         lsvsStates.setAdapter(stateAdapter);
-
+        // Evento base di un'AdapterView, serve a gestire il click su un singolo elemento della stessa.
         lsvsStates.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                /**
+                 * Recupero l'oggetto corrispondente alla cella cliccata (Oggetto nell'adapterView corrispondente alla
+                 * posiozione position) ed effettuo una conversione esplicita verso la classe a cui appartiene (String).
+                 */
                 String itemClicked = (String) parent.getItemAtPosition(position);
+                /**
+                 * Costrutto per generare un classico Toast...I parametri del metodo makeText sono:
+                 * -1) Il riferimento all'activity (MainActivity.this),
+                 * -2) Il Testo (Stinga) da mostrare all'utente,
+                 * -3) Una costante che indica il tempo di durata del Toast.
+                 * per mostrarlo richiamare infine il metodo show();
+                 *
+                 */
                 Toast.makeText(MainActivity.this, itemClicked, Toast.LENGTH_SHORT).show();
             }
         });
