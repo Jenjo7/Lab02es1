67ef6aed5b439a9b44c59e29b2bbf1df589cc55d
Gianni Savini <gianni.savini@studio.unibo.it>
2017-04-05 21:56:21 +0200

    I add OnItemLongClickListener to my ListView lsvsStates...:) I'm very happy

--- app/src/main/java/com/example/giannisavini/lab02/MainActivity.java
+++ app/src/main/java/com/example/giannisavini/lab02/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.giannisavini.lab02;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -57,15 +58,37 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, itemClicked, Toast.LENGTH_SHORT).show();
             }
         });
-        /*
+
         lsvsStates.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
                 String itemClicked = (String) parent.getItemAtPosition(position);
-                new AlertDialog().Builder(MainActivity.this)
-                return false;
+                /**
+                 * Costrutto per la creazione di un AlertDialog. Tramite la sottoclasse Builder è possibile definire diversi aspetti del dialog.
+                 * Le proprietà solitamente definite sono il titolo e il messaggio da mostrare all'utente.
+                 * E' inoltre possibile avere dei bottoni per permettere all'utente di interagire con il dialog. I bottoni possono essere definiti
+                 * "positivi" o "negativi"; in base a questa classificazione il sistema adotterà delle particolari scelte grafice e strutturali.
+                 * Per ogni bottone è definibile il testo da mostrare al suo interno, ed eventualmente un oggetto di tipo OnClickListener, per poter
+                 * intercettare il click dell'utente sul bottone stesso.
+                 */
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("Long Click!")
+                        .setMessage("Hai premuto a lungo la cella " + itemClicked)
+                        .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                Toast.makeText(MainActivity.this, "E' stato cliccato il bottone OK", Toast.LENGTH_LONG).show();
+                            }
+                        })
+                        .setNegativeButton("Annulla", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                Toast.makeText(MainActivity.this, "E' stato cliccato il bottone Annulla", Toast.LENGTH_LONG).show();
+                            }
+                        }).show();
+                return true;
             }
         });
-        */
+
     }
 }
